4f5cc81a2e7e166115fa953fa5fbf153df111cdc
sheva2103 <sheva2103@list.ru>
2024-05-16 20:41:06 +0300

    16.05.24

--- src/components/HomePage/ListMessage.tsx
+++ src/components/HomePage/ListMessage.tsx
@@ -1,5 +1,5 @@
 import styles from './HomePage.module.scss'
-import { FC, memo, useEffect, useRef, useState } from 'react';
+import { FC, memo, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import Message from './Messgae';
 import { DocumentSnapshot, doc, onSnapshot } from "firebase/firestore";
 import { db } from '../../firebase';
@@ -21,8 +21,17 @@ const ListMessages: FC<Props> = ({ selectedChat }) => {
     const isLoadChat = useAppSelector(state => state.app.loadChat)
     const listRef = useRef<HTMLDivElement>(null)
 
+    const scrollListener = () => {
+        const scrollValue = listRef.current.scrollTop
+        const listHeight = listRef.current.scrollHeight
+        const viewportHeight = listRef.current.clientHeight
+        const height = listHeight - viewportHeight
+        const scrollPercent = (scrollValue / height) * 100
+        console.log(Math.ceil(scrollPercent))
+    }
+
     useEffect(() => {
-        if(list.length) setList([])
+        if (list.length) setList([])
         const messages = onSnapshot(doc(db, "chats", selectedChat.chatID), (doc: DocumentSnapshot<Message1[]>) => {
             setList(createMessageList(doc.data()))
             if (isLoadChat) dispatch(setLoadChat())
@@ -30,42 +39,44 @@ const ListMessages: FC<Props> = ({ selectedChat }) => {
         return () => messages()
     }, [selectedChat.uid]);
 
-    // useEffect(() => {
+    useLayoutEffect(() => {
+        listRef.current?.addEventListener('scroll', scrollListener)
+        return () => {
+            console.log('снимаю слушатель с >', listRef.current)
+            listRef.current.removeEventListener('scroll', scrollListener)
+        }
+    }, []);
 
-    //     listRef.current?.addEventListener('scroll', (e) => {
-    //         const scrollValue = listRef.current.scrollTop
-    //         const listHeight = listRef.current.scrollHeight
-    //         const viewportHeight = listRef.current.clientHeight
-    //         const height = listHeight - viewportHeight
-    //         const scrollPercent = (scrollValue / height) * 100
-    //         console.log(scrollPercent)
-    //     })
-
-    // }, [listRef.current]);
-
-    if (isLoadChat) return (
-        <div className={styles.contentContainer}>
-            <div className={styles.preloaderBlock}>
-                <Preloader fontSize={'2.4rem'} />
-            </div>
-        </div>
-    )
+    // if (isLoadChat) return (
+    //     <div className={styles.contentContainer}>
+    //         <div className={styles.preloaderBlock}>
+    //             <Preloader fontSize={'2.4rem'} />
+    //         </div>
+    //     </div>
+    // )
 
     //console.log('render list messages')
 
     return (
         <div className={styles.listMessages} ref={listRef}>
-            <ul>
-                {list.map((item, index) => {
-                    if (index !== 0 && getDatefromDate(item.date) === getDatefromDate(list[index - 1].date)) {
-                        return <Message messageInfo={item} key={item.messageID} />
-                    }
-                    return <div key={item.messageID}>
-                        <GetDateMessage date={item.date} />
-                        <Message messageInfo={item} key={item.messageID} />
+            {isLoadChat ?
+                <div className={styles.contentContainer}>
+                    <div className={styles.preloaderBlock}>
+                        <Preloader fontSize={'2.4rem'} />
                     </div>
-                })}
-            </ul>
+                </div>
+                :
+                <ul>
+                    {list.map((item, index) => {
+                        if (index !== 0 && getDatefromDate(item.date) === getDatefromDate(list[index - 1].date)) {
+                            return <Message messageInfo={item} key={item.messageID} />
+                        }
+                        return <div key={item.messageID}>
+                            <GetDateMessage date={item.date} />
+                            <Message messageInfo={item} key={item.messageID} />
+                        </div>
+                    })}
+                </ul>}
         </div>
     );
 }
