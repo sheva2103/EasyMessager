f412a014e263abc8e7a9c3203fcbdf32b0ca94d0
sheva2103 <sheva2103@list.ru>
2025-02-14 19:03:12 +0200

    14.02.25 c3

--- src5/constants/constants.ts
+++
@@ -1,9 +0,0 @@
-
-export const SIGNIN = 'signIn'
-export const SIGNUP = 'signUp'
-export const FORM = 'form'
-export const BUTTONGROUP = 'buttonGroup'
-
-export const CREATE_CHANNEL = 'CREATE_CHANNEL'
-export const SETTINGS = 'SETTINGS'
-export const CONTACTS = 'CONTACTS'
